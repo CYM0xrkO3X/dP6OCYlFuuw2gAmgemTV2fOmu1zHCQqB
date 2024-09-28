7c4a9873bd91c43e42e7537e714a320db1e26438
CYM0xrkO3X <staline.hitler.dutch@gmail.com>
2024-09-28 21:03:10 +0200

    Update and rename Libraryd.lua to Library.lua

--- Library.lua
+++ Library.lua
@@ -2278,6 +2278,7 @@ do
     local KeybindLabel = Library:CreateLabel2({
         Size = UDim2.new(1, 0, 0, 20);
         Position = UDim2.fromOffset(5, 2),
+        TextXAlignment = Enum.TextXAlignment.Left,
         
         Text = 'Keybinds';
         ZIndex = 104;
